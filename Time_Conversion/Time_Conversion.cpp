63b164d578029942f3e79ce30b1a317aca825cbf
Albert <103274848+Albert311a@users.noreply.github.com>
2023-05-10 12:55:12 -0400

    Add files via upload

--- Time_Conversion/Time_Conversion.cpp
+++ Time_Conversion/Time_Conversion.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <string>
+// ven a time in -hour AM/PM format, convert it to military (24-hour) time.
+//
+// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
 std::string timeConversion(std::string s) {
     std::string sh = s, out;
